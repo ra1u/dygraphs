d2b49bbd6f3621753d6d0468e9beb5e33304086b
Luka Rahne <luka.rahne@gmail.com>
2021-05-09 12:19:26 +0200

    bump build system to more recent procedure/packages

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,7 @@ mkdir -p dist
 # Create dist/dygraph.js
 browserify \
   -v \
-  -t babelify \
-  -t [ envify --NODE_ENV development ] \
+  -t [ babelify --presets [ @babel/preset-env ] ] \
   --debug \
   --standalone Dygraph \
   src/dygraph.js \
@@ -18,27 +17,17 @@ browserify \
 # Create dist/dygraph.js.map
 cat dist/dygraph.tmp.js | exorcist --base . dist/dygraph.js.map > dist/dygraph.js
 
-# Create "production" bundle for minification
-browserify \
-  -v \
-  -t babelify \
-  -t [ envify --NODE_ENV production ] \
-  --debug \
-  --standalone Dygraph \
-  src/dygraph.js \
-  > dist/dygraph.tmp.js
+
+cp dist/dygraph.js dist/dygraph.tmp.js
+cp dist/dygraph.tmp.js dist/dygraph.tmp.js.map
 
 # Create dist/dygraph.tmp.js.map
-cat dist/dygraph.tmp.js | exorcist --base . dist/dygraph.tmp.js.map > /dev/null
+#cat dist/dygraph.tmp.js | exorcist --base . dist/dygraph.tmp.js.map > /dev/null
 
 header='/*! @license Copyright 2017 Dan Vanderkam (danvdk@gmail.com) MIT-licensed (http://opensource.org/licenses/MIT) */'
 
 # Create dist/dygraph.js.min{,.map}
 uglifyjs --compress --mangle \
-  --preamble "$header" \
-  --in-source-map dist/dygraph.tmp.js.map \
-  --source-map-include-sources \
-  --source-map dist/dygraph.min.js.map \
   -o dist/dygraph.min.js \
   dist/dygraph.tmp.js
 
@@ -50,7 +39,9 @@ cp css/dygraph.css dist/
 cleancss css/dygraph.css -o dist/dygraph.min.css --source-map --source-map-inline-sources
 
 # Build ES5-compatible distribution
+
 babel src -d src-es5 --compact false
+
 
 # Remove temp files.
 rm dist/dygraph.tmp.js
